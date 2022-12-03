3da84c84a251f73066e1d7ae8ae1e27edc0eff0c
Lionel Mason <capitalcookhouse@gmail.com>
2022-12-03 10:03:30 -0500

    commit tree f6d1d77b9238b5e9e0e4669f094e3c30dde1cd11

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -1,20 +1,66 @@
-require 'pry'
+# require 'pry'
+
+# class String
+
+#   def sentence?
+#     self.end_with?(".")
+    
+#   end
+
+#   def question?
+#     self.end_with?("?")
+
+#   end
+
+#   def exclamation?
+#     self.end_with?("!")
+
+#   end
+
+#   def count_sentences
+#     self.split(/\.|\?|!/).filter { |sentence| !sentence.empty? }.count
+#     binding.pry
+#   end
+# end
+
+
+
+
+
+
+
+
+
+
+
+
 
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
-  def count_sentences
+  
 
-  end
 end
+
+
+
+
+
+
+
+
+
+
+
+
